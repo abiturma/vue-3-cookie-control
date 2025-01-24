6383c3c1cd664f7d8c63e607ec1507ff99cd27f1
Aaron Kunert <aaron.kunert@koona-labs.de>
2025-01-24 11:32:08 +0100

    Fix bug

--- composables/useCookieControl.js
+++ composables/useCookieControl.js
@@ -1,4 +1,4 @@
-import { computed, ref } from 'vue'
+import { computed, ref, reactive } from 'vue'
 import { useLocalStorage } from "@vueuse/core";
 
 const storage = useLocalStorage('consent_control', {
